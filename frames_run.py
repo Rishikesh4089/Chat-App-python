43b478162fbde3578a3b5effc1e2ae9f466975e4
Rishikesh Prabhu <110054654+Rishikesh4089@users.noreply.github.com>
2023-04-28 09:46:01 +0530

    Update frames_run.py

--- frames_run.py
+++ frames_run.py
@@ -1,5 +1,7 @@
 import tkinter as tk
 from tkinter import ttk
+import new_encoder as nen
+import client_run as clrun
 
 
 LARGEFONT =("Verdana", 35)
@@ -75,39 +77,12 @@ class StartPage(tk.Frame):
 
 # second window frame page1
 class Page1(tk.Frame):
-	
-	def __init__(self, parent, controller):
-		
-		tk.Frame.__init__(self, parent)
-		label = ttk.Label(self, text ="Normal Chatroom", font = LARGEFONT)
-		label.grid(row = 0, column = 4, padx = 10, pady = 10)
-
-		# button to show frame 2 with text
-		# layout2
-		button1 = ttk.Button(self, text ="Home",
-							command = lambda : controller.show_frame(StartPage))
-	
-		# putting the button in its place
-		# by using grid
-		button1.grid(row = 1, column = 1, padx = 10, pady = 10)
-
+        nen.root.mainloop()
 
 
 # third window frame page2
 class Page2(tk.Frame):
-	def __init__(self, parent, controller):
-		tk.Frame.__init__(self, parent)
-		label = ttk.Label(self, text ="Encoded Chatroom", font = LARGEFONT)
-		label.grid(row = 0, column = 4, padx = 10, pady = 10)
-
-		# button to show frame 3 with text
-		# layout3
-		button2 = ttk.Button(self, text ="Home",
-							command = lambda : controller.show_frame(StartPage))
-	
-		# putting the button in its place by
-		# using grid
-		button2.grid(row = 2, column = 1, padx = 10, pady = 10)
+	clrun.main()
 
 
 # Driver Code
